174f34d9654c733b41808ee53879e77a36e56daa
seonghyeon <shp35079@naver.com>
2021-09-04 23:26:56 +0900

    [ADD] 프로필 컴포넌트 라우팅 설정

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from '../components/profile';
 
 const MainRouter = () => {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path='/profile' component={Profile}></Route>
+      </Switch>
     </Router>
   );
 };
