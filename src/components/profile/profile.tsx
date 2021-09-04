9b1e68ab523f5c761b5765067cd4cb68d600dfa7
seonghyeon <shp35079@naver.com>
2021-09-04 23:27:31 +0900

    [CREATE] 프로필 컴포넌트 초기 설정

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../footer';
+import * as S from './style';
+
+const Profile = () => {
+  return (
+    <S.Wrapper>
+      <Footer />
+    </S.Wrapper>
+  );
+};
+
+export default Profile;
