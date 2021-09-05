3a40deb6f847e6e5a96ba9786c105ddd72e78432
seonghyeon <shp35079@naver.com>
2021-09-05 22:30:25 +0900

    [ADD] UserInfo 퍼블리싱

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Footer from '../footer';
+import Header from '../header';
 import * as S from './style';
 
 const Profile = () => {
   return (
     <S.Wrapper>
+      <Header />
+      <S.UserInfoBox>
+        <S.StudentInfo>
+          <span>야예코</span>
+          <span>2103</span>
+        </S.StudentInfo>
+        <S.DormitoryInfo>
+          <span>호실</span>
+          <span>210호</span>
+        </S.DormitoryInfo>
+        <S.AccountInfo>
+          <span>계좌번호</span>
+          <span>NH농협 0102736697309</span>
+        </S.AccountInfo>
+      </S.UserInfoBox>
       <Footer />
     </S.Wrapper>
   );
--- src/components/profile/style/index.ts
+++ src/components/profile/style/index.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+export const UserInfoBox = styled.div`
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const StudentInfo = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  font-weight: bold;
+
+  & span:first-child {
+    font-size: 32px;
+  }
+
+  & span:last-child {
+    font-size: 18px;
+  }
+`;
+
+export const DormitoryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  & span:first-child {
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  & span:last-child {
+    font-size: 14px;
+  }
+`;
+
+export const AccountInfo = styled(DormitoryInfo)``;
