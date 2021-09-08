448e21798c4e8e81d9f78ffd1c9e5b3a1527a7f7
seonghyeon <shp35079@naver.com>
2021-09-09 08:38:16 +0900

    [ADD] 메인 라우터에 profile/modify 경로 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Profile from '../components/profile';
+import modifyInfo from '../components/profile/profileOptions/modifyInfo';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/profile' component={Profile}></Route>
+        <Route exact path='/profile/modify' component={modifyInfo}></Route>
       </Switch>
     </Router>
   );
