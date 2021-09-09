88de97c08bcec8308add84b66423b0d7d409d748
seonghyeon <shp35079@naver.com>
2021-09-09 20:16:36 +0900

    [ADD] modifyInfo 컴포넌트 퍼블리싱 완료

---
+++ src/components/profile/profileOptions/modifyInfo/ModifyInfo.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import OptionsTemplate from '../../../default/profile';
+import Footer from '../../../footer';
+import Header from '../../../header';
+import * as S from './style';
+import { modify } from '../../../../assets/profile';
+import { INPUT_ELEMENT } from '../../../../constance/modifyInfo';
+
+const ModifyInfo = () => {
+  return (
+    <S.Wrapper>
+      <Header />
+      <OptionsTemplate top={'320px'} image={modify} text={'내 정보 수정하기'} />
+      <S.ModifyForm>
+        {INPUT_ELEMENT.map(ele => {
+          return (
+            <S.InputWrapper key={ele.id}>
+              <input type='text' id={ele.id} required />
+              <label htmlFor={ele.id}>{ele.text}</label>
+            </S.InputWrapper>
+          );
+        })}
+        <button>확인</button>
+      </S.ModifyForm>
+      <Footer />
+    </S.Wrapper>
+  );
+};
+
+export default ModifyInfo;
