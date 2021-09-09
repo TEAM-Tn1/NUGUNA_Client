de12f27b798b84c715ca66ec4fc538dcd3f053e5
hyejun <hyejun0608@naver.com>
2021-09-09 09:16:17 +0900

    [ADD] Post components

---
+++ src/components/post/Post.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './style';
+import Header from '../header';
+import Footer from '../footer';
+
+const Post = () => {
+  return (
+    <S.Post>
+      <Header />
+      <Footer />
+    </S.Post>
+  );
+};
+
+export default Post;
---
+++ src/components/post/style/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { color } from '../../../style';
+
+export const Post = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
