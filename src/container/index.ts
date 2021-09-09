85ec4f38ba628178095e8b8bec5d47f38de13814
hyejun <hyejun0608@naver.com>
2021-09-09 09:15:26 +0900

    [ADD] post contanier

---
+++ src/container/index.ts
@@ -0,0 +1,1 @@
+export { default as PostContanier } from './post';
---
+++ src/container/post/PostContanier.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Post } from '../../components';
+
+const PostContanier = () => {
+  return <Post />;
+};
+
+export default PostContanier;
