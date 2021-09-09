990749374363d78fceb6400682ba9f52c74eb875
hyejun <hyejun0608@naver.com>
2021-09-09 09:14:01 +0900

    [ADD] trade post list route 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PostContanier } from '../container';
 
 const MainRouter = () => {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path='/post/trade' component={PostContanier} />
+      </Switch>
     </Router>
   );
 };
