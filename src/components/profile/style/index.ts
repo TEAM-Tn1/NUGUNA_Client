48686b538d76bd0d5c05cd249ddcc2dec3a43dea
seonghyeon <shp35079@naver.com>
2021-09-09 14:56:45 +0900

    [FIX] 내 정보가 잘려보이는 오류 수정

--- src/components/profile/style/index.ts
+++ src/components/profile/style/index.ts
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   max-width: 500px;
   margin: 0 auto;
+  margin-top: 100px;
 `;
 
 export const UserInfoBox = styled.div`
