7178b17213c6bfa71991259fcc31d66a18f1e000
김예진 <wlsdong0004@gmail.com>
2021-09-29 08:28:42 +0900

    [ADD] 라우터에 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PostReport, UserReport, Suggestion } from '../components/admin/index';
 
 const MainRouter = () => {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path='/admin/report/post' component={PostReport} />
+        <Route exact path='/admin/report/user' component={UserReport} />
+        <Route exact path='/admin/suggestion' component={Suggestion} />
+      </Switch>
     </Router>
   );
 };
