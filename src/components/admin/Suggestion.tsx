820ba35e07c994d3ec09675637d81fb062f31182
김예진 <wlsdong0004@gmail.com>
2021-09-28 16:46:46 +0900

    [ADD] 목록보기 UI구현

---
+++ src/components/admin/Suggestion.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import * as S from './style';
+import Sidebar from './sidebar/index';
+import List from './list/index';
+import { suggestionIcon } from '../../assets/defalut';
+
+const LIST = [
+  { id: '8', title: 'asdfasdf', target: '', name: '김박박', date: '7/6' },
+  { id: '6', title: 'dfghjkjkljhgfdhsg', target: '', name: '박김', date: '05/05' },
+  { id: '5', title: 'oiutyrew', target: '', name: '조조조', date: '12/5' },
+];
+
+const Suggestion = () => {
+  return (
+    <S.Wrapper>
+      <Sidebar />
+      <S.Main>
+        <S.Top>
+          <div>
+            <img src={suggestionIcon} />
+            <h1>문의상황</h1>
+          </div>
+          <div style={{ display: 'none'}}>
+            <p>게시물 신고</p>
+            <p style={{ opacity: 0.5 }}>유저 신고</p>
+          </div>
+        </S.Top>
+        <S.Chart>
+          <S.ChartTitle>
+            <p>번호</p>
+            <h3>제목</h3>
+            <div>
+              <p></p>
+              <p>게시자</p>
+              <p>게시일</p>
+              <p>확인 여부</p>
+            </div>
+          </S.ChartTitle>
+          <div>
+            {LIST.map(ele => {
+              return (
+                <List
+                  key={ele.id}
+                  id={ele.id}
+                  title={ele.title}
+                  target={ele.target}
+                  name={ele.name}
+                  date={ele.date}
+                />
+              );
+            })}
+          </div>
+        </S.Chart>
+      </S.Main>
+    </S.Wrapper>
+  );
+};
+
+export default Suggestion;
---
+++ src/components/admin/style/index.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { color } from '../../../style/index';
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 400px;
+`;
+
+export const Main = styled.div`
+  width: 1054px;
+  min-width: 1054px;
+  margin: 125px 80px;
+`;
+
+export const Top = styled.div`
+  width: 100%;
+  height: 66px;
+  display: flex;
+  justify-content: space-between;
+
+  & div {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & div:last-child {
+    align-items: flex-end;
+  }
+
+  & h1 {
+    color: ${color.subColor};
+    margin: 0 16px;
+  }
+
+  & p {
+    font-size: 20px;
+    font-weight: bold;
+    color: ${color.subColor};
+    margin-left: 36px;
+  }
+`;
+
+export const Chart = styled.div`
+  width: 100%;
+  margin: 50px 0;
+
+  article:nth-child(odd) {
+    background-color: #ffffff;
+  }
+
+  article:nth-child(even) {
+    background-color: rgba(117, 79, 68, 0.1);
+  }
+`;
+
+export const ChartTitle = styled.article`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin: 0 0 8px 0;
+
+  & p {
+    color: #000000;
+    font-size: 14px;
+    width: 66px;
+    display: flex;
+    justify-content: center;
+  }
+  & h3 {
+    color: #000000;
+    font-size: 22px;
+    font-weight: normal;
+    width: 708px;
+    padding: 0 18px;
+  }
+  & div {
+    width: 280px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
