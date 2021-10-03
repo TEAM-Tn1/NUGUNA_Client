2a5c244937da116d2a0a489dd9fc60643c57bcca
seonghyeon <shp35079@naver.com>
2021-10-03 23:01:04 +0900

    [ADD] 프로필 세부 페이지 애니메이션 추가

--- src/components/default/profile/OptionsTemplate.tsx
+++ src/components/default/profile/OptionsTemplate.tsx
@@ -1,15 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useHistory } from 'react-router';
 import * as S from './style';
 
 interface Props {
   top: string;
   text: string;
   image: string;
+  isShow: boolean;
+  setIsShow: any;
 }
 
-const OptionsTemplate: FC<Props> = ({ top, image, text }) => {
+const OptionsTemplate: FC<Props> = ({ top, image, text, isShow, setIsShow }) => {
+  const { push } = useHistory();
+
+  const onClick = () => {
+    setIsShow(!isShow);
+    setTimeout(() => {
+      push('/profile');
+    }, 750);
+  };
+
   return (
-    <S.Wrapper style={{ top: top }}>
+    <S.Wrapper onClick={onClick} top={top} isShow={isShow}>
       <img src={image} alt='' />
       <p>{text}</p>
     </S.Wrapper>
