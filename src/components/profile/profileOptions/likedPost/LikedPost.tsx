fc0d3552cf0c62b5542b0c3c24418e024d34de58
seonghyeon <shp35079@naver.com>
2021-10-03 23:25:35 +0900

    [ADD] LikedPost 초기 설정

--- src/components/profile/profileOptions/likedPost/LikedPost.tsx
+++ src/components/profile/profileOptions/likedPost/LikedPost.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import OptionsTemplate from '../../../default/profile';
+import Footer from '../../../footer';
+import Header from '../../../header';
+import { like } from '../../../../assets/profile';
+import * as S from './style';
 
 const LikedPost = () => {
-  return <div></div>;
+  const [isShow, setIsShow] = useState(true);
+  return (
+    <S.Wrapper>
+      <Header />
+      <OptionsTemplate
+        top={'380px'}
+        image={like}
+        text={'좋아요 표시한 게시물 보기'}
+        setIsShow={setIsShow}
+        isShow={isShow}
+      />
+      <Footer />
+    </S.Wrapper>
+  );
 };
 
 export default LikedPost;
