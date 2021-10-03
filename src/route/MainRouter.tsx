96632809f6ef0699b95a39ff98f72aed2bf0bc58
seonghyeon <shp35079@naver.com>
2021-10-03 23:25:08 +0900

    [ADD] liked_post 라우팅 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Profile from '../components/profile';
-import modifyInfo from '../components/profile/profileOptions/modifyInfo';
+import ModifyInfo from '../components/profile/profileOptions/modifyInfo';
+import LikedPost from '../components/profile/profileOptions/likedPost';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/profile' component={Profile}></Route>
-        <Route exact path='/profile/modify' component={modifyInfo}></Route>
+        <Route exact path='/profile/modify' component={ModifyInfo}></Route>
+        <Route exact path='/profile/like_post' component={LikedPost}></Route>
       </Switch>
     </Router>
   );
