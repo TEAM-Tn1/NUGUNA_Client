6a68fe7a56deb5fff9fc34a4d6560f4e7e588820
seonghyeon <shp35079@naver.com>
2021-10-04 16:38:59 +0900

    [ADD] written_post 라우팅 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Profile from '../components/profile';
 import ModifyInfo from '../components/profile/profileOptions/modifyInfo';
 import LikedPost from '../components/profile/profileOptions/likedPost';
+import WrittenPost from '../components/profile/profileOptions/writtenPost';
 
 const MainRouter = () => {
   return (
@@ -11,6 +12,7 @@ const MainRouter = () => {
         <Route exact path='/profile' component={Profile}></Route>
         <Route exact path='/profile/modify' component={ModifyInfo}></Route>
         <Route exact path='/profile/liked_post' component={LikedPost}></Route>
+        <Route exact path='profile/written_post' component={WrittenPost}></Route>
       </Switch>
     </Router>
   );
