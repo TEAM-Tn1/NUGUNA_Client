6188481049bee424fbb135e2c07c31f3d80f2346
seonghyeon <shp35079@naver.com>
2021-10-06 16:20:49 +0900

    [DELETE] 필요없는 컴포넌트 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Post } from './post';
 export { default as Footer } from './footer';
 export { default as Header } from './header';
-export { default as Logo } from './logo';
 export { default as Profile } from './profile';
 export { default as UserInfo } from './userInfo';
