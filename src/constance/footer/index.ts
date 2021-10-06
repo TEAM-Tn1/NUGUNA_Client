60e98d9fa6729f60111d3408bfbd4df5ed9dba2e
seonghyeon <shp35079@naver.com>
2021-10-06 16:12:58 +0900

    [MODIFY] 푸터 네비게이션 바 목록 이름 수정 (프로필 -> 마이페이지)

--- src/constance/footer/index.ts
+++ src/constance/footer/index.ts
@@ -3,5 +3,5 @@ export const FOOTER_TEXT = [
   { id: 'main', text: '메인', img: footerMain, url: '/main' },
   { id: 'post', text: '게시물', img: footerPost, url: '/post' },
   { id: 'chatting', text: '채팅', img: footerChatting, url: '/chatting' },
-  { id: 'profile', text: '프로필', img: footerProfile, url: '/profile' },
+  { id: 'profile', text: '마이페이지', img: footerProfile, url: '/profile' },
 ];
