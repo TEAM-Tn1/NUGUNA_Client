4973c261c3a78712e11f96539425d0feb79afa24
seonghyeon <shp35079@naver.com>
2021-10-06 15:58:59 +0900

    [CREATE] 인덱스 파일 추가

---
+++ src/components/profile/profileOptions/index.ts
@@ -0,0 +1,4 @@
+export { default as LikedPost } from './likedPost';
+export { default as ModifyInfo } from './modifyInfo';
+export { default as WrittenPost } from './writtenPost';
+// export { default as WriteSuggestion } from './writeSuggestion';
