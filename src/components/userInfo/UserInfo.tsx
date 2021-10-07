a4a24d0ae85066cb842b69d2854d9631fc6e9bb1
seonghyeon <shp35079@naver.com>
2021-10-07 12:05:05 +0900

    [ADD] 상대 프로필 퍼블리싱 틀 잡기

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { reportIcon } from '../../assets/detailPost';
 import { Header, Footer } from '../index';
 import * as S from './style';
 
@@ -6,7 +7,29 @@ const UserInfo = () => {
   return (
     <S.Wrapper>
       <Header />
-
+      <S.UserInfoBox>
+        <S.StudentInfoBox>
+          <S.StudentInfo>
+            <span>야찬코</span>
+            <span>2100</span>
+          </S.StudentInfo>
+          <S.ReportButton to=''>
+            <img src={reportIcon} alt='' />
+            <span>신고하기</span>
+          </S.ReportButton>
+        </S.StudentInfoBox>
+        <S.DormitoryInfo>
+          <span>호실</span>
+          <span>300호</span>
+        </S.DormitoryInfo>
+        <S.AccountInfo>
+          <span>계좌번호</span>
+          <span>NH농협 0102736697309</span>
+        </S.AccountInfo>
+      </S.UserInfoBox>
+      <S.WrittenPostBox>
+        <span>작성한 게시물</span>
+      </S.WrittenPostBox>
       <Footer />
     </S.Wrapper>
   );
--- src/components/userInfo/style/index.ts
+++ src/components/userInfo/style/index.ts
@@ -1,7 +1,78 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   max-width: 500px;
   margin: 0 auto;
   margin-top: 100px;
+`;
+
+export const UserInfoBox = styled.div`
+  padding: 30px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const StudentInfoBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const StudentInfo = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  font-weight: bold;
+
+  & span:first-child {
+    font-size: 32px;
+  }
+
+  & span:last-child {
+    font-size: 18px;
+  }
+`;
+
+export const ReportButton = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & img {
+    width: 20px;
+    height: 20px;
+  }
+
+  & span {
+    color: #754f44;
+    font-size: 10px;
+  }
+`;
+
+export const DormitoryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  & span:first-child {
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  & span:nth-child(2) {
+    font-size: 14px;
+  }
+`;
+
+export const AccountInfo = styled(DormitoryInfo)``;
+
+export const WrittenPostBox = styled.div`
+  & > span {
+    padding-left: 30px;
+    font-size: 18px;
+    font-weight: bold;
+  }
 `;
