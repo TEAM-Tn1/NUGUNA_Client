40f87546d682816c88306ba8c1db2a97920af73e
김예진 <wlsdong0004@gmail.com>
2021-10-09 20:40:43 +0900

    [MODIFY] Frame 안 쓰는 style 제거

--- src/components/admin/postList/frame/style/index.ts
+++ src/components/admin/postList/frame/style/index.ts
@@ -1,20 +1,6 @@
 import styled from 'styled-components';
 import { color } from '../../../../../style/index';
 
-export const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  overflow-x: scroll;
-  overflow-y: hidden;
-`;
-
-export const Main = styled.div`
-  width: 1054px;
-  min-width: 870px;
-  height: 784px;
-  margin: 125px 80px 60px 480px;
-`;
-
 export const Top = styled.div`
   width: 100%;
   height: 66px;
@@ -42,52 +28,5 @@ export const Top = styled.div`
     font-weight: bold;
     color: ${color.subColor};
     margin-left: 36px;
-  }
-`;
-
-export const Chart = styled.div`
-  width: 100%;
-  margin: 50px 0px 0px;
-
-  & div:nth-child(2) {
-    height: 628px;
-    overflow-y: scroll;
-  }
-
-  article:nth-child(odd) {
-    background-color: #ffffff;
-  }
-
-  article:nth-child(even) {
-    background-color: rgba(117, 79, 68, 0.1);
-  }
-`;
-
-export const ChartTitle = styled.article`
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  margin: 0 0 8px 0;
-  padding: 0 5px 0 0;
-
-  & p {
-    color: #000000;
-    font-size: 14px;
-    width: 66px;
-    min-width: 66px;
-    display: flex;
-    justify-content: center;
-  }
-  & h3 {
-    color: #000000;
-    font-size: 22px;
-    font-weight: normal;
-    width: 708px;
-    padding: 0 18px;
-  }
-  & div {
-    width: 280px;
-    display: flex;
-    justify-content: space-between;
   }
 `;
