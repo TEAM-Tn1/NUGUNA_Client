924d93f0d081b89d63262ba863182eb02b84d2df
hyejun <hyejun0608@naver.com>
2021-10-12 06:58:43 +0900

    [ADD] writePost component

--- src/components/writePost/style/index.ts
+++ src/components/writePost/style/index.ts
@@ -38,29 +38,124 @@ export const Title = styled.p`
   font-size: 24px;
   font-weight: bold;
   margin: 0 auto;
+  margin-bottom: 30px;
 `;
 
-export const TitleAndInput = styled.div`
+export const TitleAndInput = styled.div<{ isHashtag?: boolean }>`
   width: 86.1%;
   margin: 0 auto;
   margin-bottom: 30px;
-  > p {
-    font-size: 18px;
-    font-weight: bold;
-    margin-bottom: 10px;
-    > span {
-      font-size: 14px;
-      color: red;
-      margin-left: 4px;
-    }
-  }
   > input {
     width: 100%;
+    height: 25px;
     border: none;
+    padding: 0px 3px;
+    ${({ isHashtag }) => css`
+      border-bottom: 1px solid ${isHashtag ? color.mainColor : 'black'};
+      color: ${isHashtag ? color.mainColor : 'black'};
+    `}
+    font-size: 14px;
+    font-weight: bold;
+    ::placeholder {
+      font-size: 14px;
+      ${({ isHashtag }) => css`
+        color: ${isHashtag ? color.mainColor : '#808080'};
+      `}
+      font-weight: bold;
+    }
+  }
+`;
+
+export const SubTitle = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  > span {
+    font-size: 14px;
+    color: red;
+    margin-left: 4px;
+  }
+`;
+
+export const PictureContent = styled.div`
+  width: 86.1%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+`;
+
+export const PictureExplain = styled.p`
+  font-size: 12px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const PicturePlusBtn = styled.div`
+  width: 58px;
+  height: 58px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  > img {
+    width: 22px;
+    height: 22px;
+  }
+`;
+
+export const ExplainPostContent = styled.div`
+  width: 86.1%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+`;
+
+export const ExplainPostTextarea = styled.textarea`
+  width: 100%;
+  height: 146px;
+  background-color: #f3f3f3;
+  padding: 6px;
+  resize: none;
+  border: none;
+`;
+
+export const PostInfoContent = styled.div`
+  width: 86.1%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+  > input {
+    width: 100%;
+    height: 25px;
+    border: none;
+    padding: 0px 3px;
+    font-size: 14px;
+    font-weight: bold;
     border-bottom: 1px solid black;
     ::placeholder {
       font-size: 14px;
-      color: #808080;
+      color: '#808080';
+      font-weight: bold;
     }
+  }
+`;
+
+export const CheckBtn = styled.div<{ isClick: boolean }>`
+  width: 86.1%;
+  height: 68px;
+  margin: 0 auto;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${({ isClick }) => css`
+    background-color: ${isClick ? '#754f44' : 'white'};
+  `}
+  border: 1px solid #754f44;
+  margin-bottom: 47px;
+  > p {
+    font-size: 20px;
+    font-weight: bold;
+    ${({ isClick }) => css`
+      color: ${isClick ? 'white' : '#754f44'};
+    `}
   }
 `;
