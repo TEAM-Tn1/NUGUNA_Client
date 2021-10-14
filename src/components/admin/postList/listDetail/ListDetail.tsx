28a3a1386ad8b284ed098ff69ca17d97681d299d
김예진 <wlsdong0004@gmail.com>
2021-10-14 20:24:59 +0900

    [MODIFY] 비활설화일 때 정지일 클릭 못하게하기

--- src/components/admin/postList/listDetail/ListDetail.tsx
+++ src/components/admin/postList/listDetail/ListDetail.tsx
@@ -11,11 +11,7 @@ const ListDetail: FC<detailProps> = ({ description, photo_url }) => {
   const [whether, setWhether] = useState<boolean>(true);
 
   const [displayOpcity, setDisplayOpcity] = useState<number>(1);
-  const [displayCilck, setDisplayCilck] = useState<string>('auto');
-  /* const styles = {
-    opcity: displayOpcity,
-    'pointer-events': displayCilck,
-  }; */
+  const [displayCilck, setDisplayCilck] = useState<any>('auto');
 
   useEffect(() => {
     whether ? setDisplayOpcity(1) : setDisplayOpcity(0.3);
@@ -36,7 +32,7 @@ const ListDetail: FC<detailProps> = ({ description, photo_url }) => {
         <div>
           <p>유저 비활성화 여부</p>
           <img src={whether ? positive : negative} onClick={() => setWhether(!whether)} />
-          <input type='date' /*  style={styles} */ />
+          <input type='date' style={{ opacity: displayOpcity, pointerEvents: displayCilck }} />
         </div>
         <textarea placeholder='답변을 남겨주세요.' />
         <div>
