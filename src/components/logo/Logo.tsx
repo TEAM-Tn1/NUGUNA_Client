b1fa217721db95f1b788eac16534215d14ec8910
김예진 <wlsdong0004@gmail.com>
2021-10-15 12:39:50 +0900

    [CREATE] Logo 퍼블리싱

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as S from './style';
+import { chickLogo } from '../../assets/logo';
+
+const Logo = () => {
+  return (
+    <S.Logo>
+      <img src={chickLogo} />
+      <p>
+        <span>누</span>구나
+        <br />
+        <span>구</span>매하고
+        <br />
+        <span>나</span>누자
+      </p>
+    </S.Logo>
+  );
+};
+
+export default Logo;
