8c9f38c8f4ee41ecfa37bc1e3ad84adb2a280c5a
김예진 <wlsdong0004@gmail.com>
2021-10-15 11:21:56 +0900

    [MODIFY] 억지로 변경사항 만들기

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,18 +3,18 @@ import * as S from './style';
 import { chickLogo } from '../../assets/logo';
 
 const Logo = () => {
-    return (
-        <S.Logo>
-        <img src={chickLogo} />
-        <p>
-          <span>누</span>구나
-          <br />
-          <span>구</span>매하고
-          <br />
-          <span>나</span>누자
-        </p>
-      </S.Logo>
-    );
+  return (
+    <S.Logo>
+      <img src={chickLogo} alt='' />
+      <p>
+        <span>누</span>구나
+        <br />
+        <span>구</span>매하고
+        <br />
+        <span>나</span>누자
+      </p>
+    </S.Logo>
+  );
 };
 
 export default Logo;
