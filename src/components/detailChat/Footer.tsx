0404c2c95cd4edd3c70f6cd8dcda03fccbc8463a
hyejun <hyejun0608@naver.com>
2021-10-17 20:27:45 +0900

    [ADD] setting 라인 추가

--- src/components/detailChat/Footer.tsx
+++ src/components/detailChat/Footer.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from './style';
 import { setting, send } from '../../assets/chat';
+import { SETTING } from '../../constance/detailChat';
 
-const Footer = () => {
+interface Props {
+  isClickSettingBtn: boolean;
+  setIsClickSettingBtn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Footer: FC<Props> = props => {
+  const { setIsClickSettingBtn, isClickSettingBtn } = props;
+
+  const showSetting = useMemo(() => {
+    if (isClickSettingBtn)
+      return (
+        <S.SettingLine>
+          {SETTING.map(data => {
+            return (
+              <div key={data.id}>
+                <img src={data.img} alt={data.id} />
+                <p>{data.content}</p>
+              </div>
+            );
+          })}
+        </S.SettingLine>
+      );
+  }, [isClickSettingBtn]);
+
+  const settingBtnClickHandler = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    setIsClickSettingBtn(!isClickSettingBtn);
+    if (isClickSettingBtn) event.currentTarget.style.transform = 'rotate(0deg)';
+    else event.currentTarget.style.transform = 'rotate(45deg)';
+  };
+
   return (
     <S.FooterBox>
       <S.FooterWrapper>
-        <img src={setting} alt='setting' />
+        {showSetting}
+        <img src={setting} alt='setting' onClick={settingBtnClickHandler} />
         <S.ChatInput />
         <img src={send} alt='send' />
       </S.FooterWrapper>
