8d934461473e5ead745101a46235f86bbf66fa50
hyejun <hyejun0608@naver.com>
2021-10-17 20:27:34 +0900

    [ADD] people props 추가

--- src/components/detailChat/Header.tsx
+++ src/components/detailChat/Header.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from './style';
 import { prev } from '../../assets/chat';
 
-const Header = () => {
+interface Props {
+  type: string;
+}
+
+const Header: FC<Props> = props => {
+  const { type } = props;
+
+  const people = useMemo(() => {
+    if (type === 'group') return <span>4</span>;
+    else return;
+  }, [type]);
+
   return (
     <S.Header>
       <S.HeaderWrapper>
         <img src={prev} alt='prev' />
         <p>일이삼사오육칠팔구십</p>
-        <span>4</span>
+        {people}
       </S.HeaderWrapper>
     </S.Header>
   );
