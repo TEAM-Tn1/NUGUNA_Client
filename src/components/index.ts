e752e77f1ac91ed4c94f8f7e2712e24a1044f976
seonghyeon <shp35079@naver.com>
2021-10-17 23:15:32 +0900

    [ADD] information 컴포넌트 인덱스 파일 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Header } from './header';
 export { default as Mypage } from './mypage';
 export { default as UserInfo } from './userInfo';
 export { default as Auth } from './auth';
+export { default as Information } from './information';
