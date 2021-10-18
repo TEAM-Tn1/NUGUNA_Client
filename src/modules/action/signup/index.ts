72a947aa8ec4d7e660935b9fe26a9fdc458034b4
hyejun <hyejun0608@naver.com>
2021-10-18 10:20:28 +0900

    [DELETE] 예시 코드 삭제

--- src/modules/action/signup/index.ts
+++
@@ -1,6 +0,0 @@
-import { CODE } from './interface';
-import { createAction } from 'typesafe-actions';
-
-export const setCode = createAction(CODE)<string>();
-
-export type signupActionType = ReturnType<typeof setCode>;
