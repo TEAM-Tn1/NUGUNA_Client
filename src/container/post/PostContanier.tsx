50604653c846aa842835bd4e0780b1dbc5fc2d5c
hyejun <hyejun0608@naver.com>
2021-10-19 15:02:27 +0900

    [ADD] post container

--- src/container/post/PostContanier.tsx
+++ src/container/post/PostContanier.tsx
@@ -1,10 +1,21 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Post } from '../../components';
+import { CARROT_POSTLIST, GROUP_POSTLIST } from '../../modules/action/post/interface';
+import usePost from '../../util/hooks/post';
 
 const PostContanier: FC = () => {
+  const { state, setState } = usePost();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (state.type === 'trade') dispatch({ type: CARROT_POSTLIST });
+    else if (state.type === 'group') dispatch({ type: GROUP_POSTLIST });
+  }, [state.type, state.page, state.order]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Post />
+      <Post {...state} {...setState} />
     </Suspense>
   );
 };
