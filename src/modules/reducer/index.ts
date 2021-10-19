b1fb4b7022602b8bddd63195cb608f7501162bff
hyejun <hyejun0608@naver.com>
2021-10-19 15:01:19 +0900

    [ADD] post redux reducer

--- src/modules/reducer/index.ts
+++ src/modules/reducer/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import postReducer from './post';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ post: postReducer });
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
---
+++ src/modules/reducer/post/index.ts
@@ -0,0 +1,84 @@
+import PostState from './interface';
+import { postActionType } from '../../action/post';
+import {
+  ORDER,
+  TYPE,
+  PAGE,
+  CARROT_POSTLIST,
+  CARROT_POSTLIST_SUCCESS,
+  CARROT_POSTLIST_FAILURE,
+  GROUP_POSTLIST,
+  GROUP_POSTLIST_SUCCESS,
+  GROUP_POSTLIST_FAILURE,
+} from '../../action/post/interface';
+
+const initState: PostState = {
+  postList: [],
+  type: 'trade',
+  order: { newest: true, like: false },
+  typeClick: { trade: true, group: false },
+  page: 1,
+  isSuccessGetPostList: undefined,
+  error: {
+    status: 0,
+    message: '',
+    type: '',
+  },
+};
+
+const postReducer = (state: PostState = initState, action: postActionType): PostState => {
+  switch (action.type) {
+    case CARROT_POSTLIST:
+      return {
+        ...state,
+        isSuccessGetPostList: undefined,
+      };
+    case CARROT_POSTLIST_SUCCESS:
+      return {
+        ...state,
+        postList: action.payload.postList,
+        isSuccessGetPostList: true,
+      };
+    case CARROT_POSTLIST_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isSuccessGetPostList: false,
+      };
+    case GROUP_POSTLIST:
+      return {
+        ...state,
+        isSuccessGetPostList: undefined,
+      };
+    case GROUP_POSTLIST_SUCCESS:
+      return {
+        ...state,
+        postList: action.payload.postList,
+        isSuccessGetPostList: true,
+      };
+    case GROUP_POSTLIST_FAILURE:
+      return {
+        ...state,
+        isSuccessGetPostList: false,
+      };
+    case TYPE:
+      return {
+        ...state,
+        type: action.payload,
+      };
+    case ORDER:
+      return {
+        ...state,
+        order: action.payload,
+      };
+    case PAGE:
+      return {
+        ...state,
+        page: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
