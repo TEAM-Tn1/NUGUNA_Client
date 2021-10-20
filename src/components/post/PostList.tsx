f0e3c1e7f4284036daacfe4e11b81cb113063b6e
hyejun <hyejun0608@naver.com>
2021-10-20 10:43:33 +0900

    [FIX] 무한 스크롤 기능 설정

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,33 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import * as S from './style';
 import PostContent from './PostContent';
 import { postListType } from '../../models/dto/response/postResponse';
+import { useInView } from 'react-intersection-observer';
 
 interface Props {
   type: string;
+  page: number;
   postList: Array<postListType>;
+  setPage: (payload: number) => void;
 }
 
 const PostList: FC<Props> = props => {
-  const { type, postList } = props;
+  const { type, postList, page, setPage } = props;
+  const [loading, setLoading] = useState<boolean>(false);
+  const totalpage = 5;
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (postList.length !== 0) {
+      if (!loading) {
+        setLoading(true);
+        if (page + 1 < totalpage) {
+          setPage(page + 1);
+          setLoading(false);
+        }
+      }
+    }
+  }, [inView]);
+
   return (
     <S.PostList>
-      {postList.map(data => {
-        const date = data.last_modify_date;
-        const people = `${data.current_cout}/${data.head_count}`;
-        const hashtag = data.tags.split(',');
-        return (
-          <PostContent
-            title={data.title}
-            money={data.price}
-            like={data.count}
-            type={type}
-            date={date}
-            people={people}
-            hashtag={hashtag}
-          />
-        );
-      })}
+      {postList &&
+        postList.map(data => {
+          const date = data.last_modify_date;
+          const people = `${data.current_cout}/${data.head_count}`;
+          const hashtag = data.tags.split(',');
+          return (
+            <PostContent
+              title={data.title}
+              money={data.price}
+              like={data.count}
+              type={type}
+              date={date}
+              people={people}
+              hashtag={hashtag}
+              isLikeClick={data.like}
+            />
+          );
+        })}
+      {!loading && <div ref={ref} />}
     </S.PostList>
   );
 };
