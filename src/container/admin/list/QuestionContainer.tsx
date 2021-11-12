9b42e2f89453ef0291c647e462cd2ccb49cd5a9b
김예진 <wlsdong0004@gmail.com>
2021-11-12 09:42:16 +0900

    [FIX] container 수정

--- src/container/admin/list/QuestionContainer.tsx
+++ src/container/admin/list/QuestionContainer.tsx
@@ -2,10 +2,10 @@ import React, { FC, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Suggestion } from '../../../components/admin';
 import { QUESTION_LIST } from '../../../modules/action/admin/interface';
-import { useAdminList } from '../../../util/hooks/admin';
+import { useAdminQuestionList } from '../../../util/hooks/admin';
 
 const QuestionContainer: FC = () => {
-  const { state, setState } = useAdminList();
+  const { state, setState } = useAdminQuestionList();
   const dispatch = useDispatch();
 
   useEffect(() => {
--- src/container/admin/list/ReportUserContainer.tsx
+++ src/container/admin/list/ReportUserContainer.tsx
@@ -2,10 +2,10 @@ import React, { FC, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { UserReport } from '../../../components/admin';
 import { REPORT_USER_LIST } from '../../../modules/action/admin/interface';
-import { useAdminList } from '../../../util/hooks/admin';
+import { useAdminReportUserList } from '../../../util/hooks/admin';
 
 const ReportUserContainer = () => {
-  const { state, setState } = useAdminList();
+  const { state, setState } = useAdminReportUserList();
   const dispatch = useDispatch();
 
   useEffect(() => {
