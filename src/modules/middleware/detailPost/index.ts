ea2a493bf82e0f26f39334d19e7e188925c12cbf
hyejun <hyejun0608@naver.com>
2021-11-15 09:36:44 +0900

    [ADD] 좋아요 등록 & 삭제 saga

--- src/modules/middleware/detailPost/index.ts
+++ src/modules/middleware/detailPost/index.ts
@@ -1,6 +1,6 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { getDetailPost } from '../../../util/api/detailPost';
-import { GET_POST } from '../../action/detailPost/interface';
+import { getDetailPost, postLike, postLikeDelete } from '../../../util/api/detailPost';
+import { GET_POST, POST_LIKE, POST_LIKE_DELETE } from '../../action/detailPost/interface';
 import { reducerType } from '../../reducer';
 import DetailPostState from '../../reducer/detailPost/interface';
 
@@ -37,8 +37,72 @@ const detailPostGetSaga = function* (): any {
   }
 };
 
+const postLikeSaga = function* (): any {
+  const actionType = 'DETAILPOST/POST_LIKE';
+  const SUCCESS = `${actionType}_SUCCESS`;
+  const FAILURE = `${actionType}_FAILURE`;
+  const state = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(postLike, accessToken, state.feedId);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: actionType },
+      });
+    } else {
+      console.log(error);
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
+const postLikeDeleteSaga = function* (): any {
+  const actionType = 'DETAILPOST/POST_LIKE_DELETE';
+  const SUCCESS = `${actionType}_SUCCESS`;
+  const FAILURE = `${actionType}_FAILURE`;
+  const state = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(postLikeDelete, accessToken, state.feedId);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: actionType },
+      });
+    } else {
+      console.log(error);
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
 function* detailPostSaga() {
   yield takeLatest(GET_POST, detailPostGetSaga);
+  yield takeLatest(POST_LIKE, postLikeSaga);
+  yield takeLatest(POST_LIKE_DELETE, postLikeDeleteSaga);
 }
 
 export default detailPostSaga;
