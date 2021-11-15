26f109bbc9cd30a6a00feb979cffa79f83e20a07
hyejun <hyejun0608@naver.com>
2021-11-15 09:36:57 +0900

    [ADD] 좋아요 등록 & 삭제 action

--- src/modules/action/detailPost/index.ts
+++ src/modules/action/detailPost/index.ts
@@ -1,15 +1,38 @@
 import { createAction } from 'typesafe-actions';
 import { detailPostResponse } from '../../../models/dto/response/detailPostResponse';
 import { error } from '../../../models/error';
-import { FEED_ID, GET_POST, GET_POST_FAILURE, GET_POST_SUCCESS } from './interface';
+import {
+  FEED_ID,
+  GET_POST,
+  GET_POST_FAILURE,
+  GET_POST_SUCCESS,
+  POST_LIKE,
+  POST_LIKE_DELETE,
+  POST_LIKE_DELETE_FAILURE,
+  POST_LIKE_DELETE_SUCCESS,
+  POST_LIKE_FAILURE,
+  POST_LIKE_SUCCESS,
+} from './interface';
 
 export const saveFeedId = createAction(FEED_ID)<number>();
 export const getPost = createAction(GET_POST)();
 export const getPostSuccess = createAction(GET_POST_SUCCESS)<detailPostResponse>();
 export const getPostFailure = createAction(GET_POST_FAILURE)<error>();
+export const postLike = createAction(POST_LIKE)();
+export const postLikeSuccess = createAction(POST_LIKE_SUCCESS)();
+export const postLikeFailure = createAction(POST_LIKE_FAILURE)<error>();
+export const postLikeDelete = createAction(POST_LIKE_DELETE)();
+export const postLikeDeleteSuccess = createAction(POST_LIKE_DELETE_SUCCESS)();
+export const postLikeDeleteFailure = createAction(POST_LIKE_DELETE_FAILURE)<error>();
 
 export type detailPostActionType =
   | ReturnType<typeof saveFeedId>
   | ReturnType<typeof getPost>
   | ReturnType<typeof getPostSuccess>
-  | ReturnType<typeof getPostFailure>;
+  | ReturnType<typeof getPostFailure>
+  | ReturnType<typeof postLike>
+  | ReturnType<typeof postLikeSuccess>
+  | ReturnType<typeof postLikeFailure>
+  | ReturnType<typeof postLikeDelete>
+  | ReturnType<typeof postLikeDeleteSuccess>
+  | ReturnType<typeof postLikeDeleteFailure>;
