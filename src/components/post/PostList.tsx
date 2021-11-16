d4aaecc41aaab5a103b36b440fb9ce27fa6d82e3
hyejun <hyejun0608@naver.com>
2021-11-17 00:05:09 +0900

    [FIX] 게시글을 눌렀을 때 해당 게시글의 상세보기 페이지로 넘어감

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -5,6 +5,7 @@ import PostContent from './PostContent';
 import { postListType } from '../../models/dto/response/postResponse';
 import { useInView } from 'react-intersection-observer';
 import { chickLogo } from '../../assets/logo';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
   type: string;
@@ -57,6 +58,7 @@ const PostList: FC<Props> = props => {
           const showPeople = `${data.current_head_count}/${data.head_count}`;
           return (
             <PostContent
+              feedId={data.feed_id}
               medium={data.medium}
               title={data.title}
               money={data.price}
