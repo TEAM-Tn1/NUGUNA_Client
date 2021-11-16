5fc5ba05b10088814eedd8affe226d2ea77e49e1
seonghyeon <shp35079@naver.com>
2021-11-16 23:24:31 +0900

    [ADD] 내 정보 수정하기 api 연동

--- src/util/api/mypage/index.ts
+++ src/util/api/mypage/index.ts
@@ -51,4 +51,17 @@ export default {
       },
     });
   },
+  modifyInfo(room_number: string, account_number: string) {
+    return request({
+      url: `users/information`,
+      method: 'patch',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+      data: {
+        room_number,
+        account_number,
+      },
+    });
+  },
 };
