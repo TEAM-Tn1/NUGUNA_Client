8999de3d549348099f8bc55fac66f1414754621b
hyejun <hyejun0608@naver.com>
2021-11-17 11:14:59 +0900

    [ADD] input change handler 추가

--- src/components/writePost/ExplainPost.tsx
+++ src/components/writePost/ExplainPost.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './style';
 import { EXPLAINPOSTTEXTAREA, EXPLAINPOSTTITLE } from '../../constance/writePost';
 
-const ExplainPost = () => {
+interface Props {
+  setDescription: (payload: string) => void;
+}
+
+const ExplainPost: FC<Props> = props => {
+  const { setDescription } = props;
+
+  const textareaChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.currentTarget.value);
+  };
+
   return (
     <S.ExplainPostContent>
       <S.SubTitle>
         {EXPLAINPOSTTITLE}
         <span>*</span>
       </S.SubTitle>
-      <S.ExplainPostTextarea placeholder={EXPLAINPOSTTEXTAREA} />
+      <S.ExplainPostTextarea placeholder={EXPLAINPOSTTEXTAREA} onChange={textareaChangeHandler} />
     </S.ExplainPostContent>
   );
 };
