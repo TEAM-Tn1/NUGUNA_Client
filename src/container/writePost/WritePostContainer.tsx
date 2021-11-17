723abbddb744cc9a628ba83ce13908ccd519756d
hyejun <hyejun0608@naver.com>
2021-11-17 11:14:01 +0900

    [ADD] writePostContainer

--- src/container/writePost/WritePostContainer.tsx
+++ src/container/writePost/WritePostContainer.tsx
@@ -1,10 +1,12 @@
 import React, { FC, Suspense } from 'react';
 import WritePost from '../../components/writePost';
+import useWritePost from '../../util/hooks/writePost';
 
 const WritePostContanier: FC = () => {
+  const { state, setState } = useWritePost();
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <WritePost />
+      <WritePost {...state} {...setState} />
     </Suspense>
   );
 };
