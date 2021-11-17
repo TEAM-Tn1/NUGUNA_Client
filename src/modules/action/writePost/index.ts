f809378aac1b2e3c3136ceda138ed4c5b3a87ba2
hyejun <hyejun0608@naver.com>
2021-11-18 01:32:01 +0900

    [ADD] 이미지 추가 action

--- src/modules/action/writePost/index.ts
+++ src/modules/action/writePost/index.ts
@@ -13,6 +13,11 @@ import {
   GROUP,
   GROUP_SUCCESS,
   GROUP_FAILURE,
+  PICTURE,
+  PICTURE_SUCCESS,
+  PICTURE_FAILURE,
+  IMG,
+  FEEDID,
 } from './interface';
 
 export const setTitle = createAction(TITLE)<string>();
@@ -21,12 +26,17 @@ export const setPrice = createAction(PRICE)<number>();
 export const setTags = createAction(TAGS)<Array<string>>();
 export const setDate = createAction(DATE)<string>();
 export const setHeadCount = createAction(HEADCOUNT)<number>();
+export const setImg = createAction(IMG)<Array<File>>();
+export const setFeedId = createAction(FEEDID)<number>();
 export const carrot = createAction(CARROT)();
-export const carrotSuccess = createAction(CARROT_SUCCESS)();
+export const carrotSuccess = createAction(CARROT_SUCCESS)<{ feed_id: number }>();
 export const carrotFailure = createAction(CARROT_FAILURE)<error>();
 export const group = createAction(GROUP)();
-export const groupSuccess = createAction(GROUP_SUCCESS)();
+export const groupSuccess = createAction(GROUP_SUCCESS)<{ feed_id: number }>();
 export const groupFailure = createAction(GROUP_FAILURE)<error>();
+export const picture = createAction(PICTURE)();
+export const pictureSuccess = createAction(PICTURE_SUCCESS)();
+export const pictureFailure = createAction(PICTURE_FAILURE)<error>();
 
 export type writePostActionType =
   | ReturnType<typeof setTitle>
@@ -35,9 +45,14 @@ export type writePostActionType =
   | ReturnType<typeof setTags>
   | ReturnType<typeof setDate>
   | ReturnType<typeof setHeadCount>
+  | ReturnType<typeof setImg>
+  | ReturnType<typeof setFeedId>
   | ReturnType<typeof carrot>
   | ReturnType<typeof carrotSuccess>
   | ReturnType<typeof carrotFailure>
   | ReturnType<typeof group>
   | ReturnType<typeof groupSuccess>
-  | ReturnType<typeof groupFailure>;
+  | ReturnType<typeof groupFailure>
+  | ReturnType<typeof picture>
+  | ReturnType<typeof pictureSuccess>
+  | ReturnType<typeof pictureFailure>;
