a83dcc1eb3588f35ebe0fe1c8725b8b4f8ab2d4b
김예진 <wlsdong0004@gmail.com>
2021-11-18 14:27:39 +0900

    [FIX] date slice

--- src/components/admin/postList/list/List.tsx
+++ src/components/admin/postList/list/List.tsx
@@ -1,39 +1,70 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC, useState, useEffect } from 'react';
 import { CheckIcon } from '../../../../assets/admin';
+import { ListDetail } from '../listDetail';
 import * as S from './style/index';
 
 interface listProps {
   openDetail: () => void;
+  displayBool: boolean;
   postId: number | string;
   title: string;
   target: string;
   writer: string;
   date: string;
   check: boolean;
+  option: number;
 }
 
-const List: FC<listProps> = ({ openDetail, postId, title, target, writer, date, check }) => {
+const List = ({
+  openDetail,
+  displayBool,
+  postId,
+  title,
+  target,
+  writer,
+  date,
+  check,
+  option,
+}: listProps) => {
   const [color, setColor] = useState<string>('');
   useEffect(() => {
     check ? setColor('#00C02A') : setColor('#808080');
   }, []);
 
+  const [divDisplayBool, setDivDisplayBool] = useState<boolean>(false);
+  const showDetail = () => {
+    setDivDisplayBool(!divDisplayBool);
+  };
+  const dateMonth = date.slice(5, 7);
+  const dateDay = date.slice(8, 10);
+
+  const dateString = dateMonth + '/' + dateDay;
+
   return (
-    <article onClick={openDetail}>
-      <S.MainContest>
-        <p>{postId}</p>
-        <h3>{title}</h3>
-        <div>
-          <p>{target}</p>
-          <p>{writer}</p>
-          <p>{date}</p>
+    <>
+      <article onClick={openDetail}>
+        <S.MainContest>
+          <p>{postId}</p>
+          <h3>{title}</h3>
           <div>
-            <CheckIcon color={color} />
+            <p>{target}</p>
+            <p>{writer}</p>
+            <p>{dateString}</p>
+            <div>
+              <CheckIcon color={color} />
+            </div>
           </div>
-        </div>
-      </S.MainContest>
-    </article>
+        </S.MainContest>
+      </article>
+      <ListDetail
+        closeDetail={showDetail}
+        description={''}
+        photo_url={''}
+        option={option}
+        styles={displayBool}
+      />
+    </>
   );
 };
 
