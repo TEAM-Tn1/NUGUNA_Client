775e8225b822ad0703e19fee81a23f1fe354de84
hyejun <hyejun0608@naver.com>
2021-11-20 23:39:24 +0900

    [ADD] chatList middleware

--- src/modules/middleware/index.ts
+++ src/modules/middleware/index.ts
@@ -3,7 +3,8 @@ import post from './post';
 import detailPost from './detailPost';
 import adminListSaga from './admin/list';
 import writePostSaga from './writePost';
+import chatListSaga from './chatList';
 
 export default function* rootSaga() {
-  yield all([post(), detailPost(), adminListSaga(), writePostSaga()]);
+  yield all([post(), detailPost(), adminListSaga(), writePostSaga(), chatListSaga()]);
 }
