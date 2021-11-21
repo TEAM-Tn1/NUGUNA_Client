87dafc7a4ce70364268b30d3c7fdcada0f505862
hyejun <hyejun0608@naver.com>
2021-11-21 21:14:44 +0900

    [ADD] GROUP_CHAT 추가

--- src/modules/reducer/chatList/index.ts
+++ src/modules/reducer/chatList/index.ts
@@ -3,6 +3,9 @@ import {
   CARROT_CHAT,
   CARROT_CHAT_FAILURE,
   CARROT_CHAT_SUCCESS,
+  GROUP_CHAT,
+  GROUP_CHAT_FAILURE,
+  GROUP_CHAT_SUCCESS,
 } from '../../action/chatList/interface';
 import ChatListState from './interface';
 
@@ -33,6 +36,23 @@ const chatListReducer = (
         isSuccessGetChatList: true,
       };
     case CARROT_CHAT_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isSuccessGetChatList: false,
+      };
+    case GROUP_CHAT:
+      return {
+        ...state,
+        isSuccessGetChatList: undefined,
+      };
+    case GROUP_CHAT_SUCCESS:
+      return {
+        ...state,
+        chatList: action.payload,
+        isSuccessGetChatList: true,
+      };
+    case GROUP_CHAT_FAILURE:
       return {
         ...state,
         error: action.payload,
