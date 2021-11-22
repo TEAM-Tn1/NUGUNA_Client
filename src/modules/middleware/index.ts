018b2467df8ab98de95eb73d613b4bc8ddbbc1e9
hyejun <hyejun0608@naver.com>
2021-11-23 00:06:26 +0900

    [ADD] search middleware

--- src/modules/middleware/index.ts
+++ src/modules/middleware/index.ts
@@ -4,7 +4,8 @@ import detailPost from './detailPost';
 import adminListSaga from './admin/list';
 import writePostSaga from './writePost';
 import chatListSaga from './chatList';
+import searchSaga from './search';
 
 export default function* rootSaga() {
-  yield all([post(), detailPost(), adminListSaga(), writePostSaga(), chatListSaga()]);
+  yield all([post(), detailPost(), adminListSaga(), writePostSaga(), chatListSaga(), searchSaga()]);
 }
