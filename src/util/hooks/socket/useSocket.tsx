f52f32594b9571fbee8863557bcb4019262fae63
hyejun <hyejun0608@naver.com>
2021-11-24 00:53:13 +0900

    [FIX] any 타입이니 props로 끌어쓸 때 emit & on 이 사용이 안됨

--- src/util/hooks/socket/useSocket.tsx
+++ src/util/hooks/socket/useSocket.tsx
@@ -2,16 +2,18 @@ import { useEffect, useRef } from 'react';
 import socketIO from 'socket.io-client';
 
 export const useSocket = () => {
-  const socket = useRef<any>();
-  const SOCKET_SEVER_URL = `https://server.tn1-dsm.com/Authorization=${localStorage.getItem(
+  const socket = useRef<SocketIOClient.Socket>();
+  const SOCKET_SEVER_URL = `https://server.tn1-dsm.com?Authorization=${localStorage.getItem(
     'access_token',
   )}`;
 
   useEffect(() => {
+    console.log('test2');
     socket.current = socketIO.connect(SOCKET_SEVER_URL, {
       transports: ['websocket'],
     });
-  }, []);
+    return () => console.log('test');
+  }, [SOCKET_SEVER_URL, socket]);
 
   return { socket } as const;
 };
