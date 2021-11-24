086ed2b1d8cb592a2e0efa825f52b39be327fc0b
hyejun <hyejun0608@naver.com>
2021-11-24 23:28:31 +0900

    [ADD] detailChat api

--- src/util/api/detailChat/index.ts
+++ src/util/api/detailChat/index.ts
@@ -1,7 +1,7 @@
 import uri from '../../../constance/uri';
 import { getRequestWithAccessToken } from '../default';
 
-export const getDetailPost = async (accessToken: string, roomId: string, page: number) => {
+export const getDetailChat = async (accessToken: string, roomId: string, page: number) => {
   try {
     const request = getRequestWithAccessToken(accessToken);
     return await request.get(`${uri.content}?page=${page}`);
