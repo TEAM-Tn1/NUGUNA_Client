edfb5d9cd1ce9107beaefb1893f16c851059646b
hyejun <hyejun0608@naver.com>
2021-11-24 21:39:59 +0900

    [ADD] 검색 api 활성화

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import * as S from './style';
 import { search, alram } from '../../assets/header';
+import useSearch from '../../util/hooks/search';
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { SEARCH } from '../../modules/action/search/interface';
 
 const Header = () => {
+  const { state, setState } = useSearch();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState.setTitle(event.currentTarget.value);
+  };
+
+  const inputIconClickHandler = () => {
+    history.push('/search');
+    dispatch({ type: SEARCH });
+  };
+
   return (
     <S.Header>
       <S.Wrapper>
         <S.SearchInputBox>
-          <S.SearchImg src={search} />
-          <S.SearchInput type='text' />
+          <S.SearchImg src={search} onClick={inputIconClickHandler} />
+          <S.SearchInput type='text' onChange={inputChangeHandler} defaultValue={state.title} />
         </S.SearchInputBox>
         <S.AlramBox>
           <S.AlramImg src={alram} />
