3bf58602046cd40aecff07af658fdd754492521a
hyejun <hyejun0608@naver.com>
2021-11-25 01:51:45 +0900

    [FIX] ? -> &

--- src/util/api/detailChat/index.ts
+++ src/util/api/detailChat/index.ts
@@ -4,7 +4,7 @@ import { getRequestWithAccessToken } from '../default';
 export const getDetailChat = async (accessToken: string, roomId: string, page: number) => {
   try {
     const request = getRequestWithAccessToken(accessToken);
-    return await request.get(`${uri.content}?page=${page}?room_id=${roomId}`);
+    return await request.get(`${uri.content}?page=${page}&room_id=${roomId}`);
   } catch (error) {
     throw error;
   }
