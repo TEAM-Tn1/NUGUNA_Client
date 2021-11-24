57f773b9e224a7b74021b8bb09cd3b2f31184a6f
hyejun <hyejun0608@naver.com>
2021-11-24 23:30:44 +0900

    [FIX] 대충 예상해서 쿼리 스트링 넣어봤어요

--- src/util/api/detailChat/index.ts
+++ src/util/api/detailChat/index.ts
@@ -4,7 +4,7 @@ import { getRequestWithAccessToken } from '../default';
 export const getDetailChat = async (accessToken: string, roomId: string, page: number) => {
   try {
     const request = getRequestWithAccessToken(accessToken);
-    return await request.get(`${uri.content}?page=${page}`);
+    return await request.get(`${uri.content}?page=${page}?roomId=${roomId}`);
   } catch (error) {
     throw error;
   }
