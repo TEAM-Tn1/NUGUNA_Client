4e4e63fddf0b3bd7e9bba79fea2de3c92d3937e4
hyejun <hyejun0608@naver.com>
2021-11-25 22:57:49 +0900

    [DELETE] 안쓰는거 삭제

--- src/modules/middleware/writePost/index.ts
+++ src/modules/middleware/writePost/index.ts
@@ -1,4 +1,4 @@
-import { call, put, select, take, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { getDetailPost } from '../../../util/api/detailPost';
 import {
   writeCarrotPost,
