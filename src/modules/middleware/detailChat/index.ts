481466b19051108355a4f92ac15dc87999083dd4
hyejun <hyejun0608@naver.com>
2021-11-28 22:55:14 +0900

    [ADD] chatInfoGetSaga

--- src/modules/middleware/detailChat/index.ts
+++ src/modules/middleware/detailChat/index.ts
@@ -1,6 +1,6 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { getDetailChat, getInfo } from '../../../util/api/detailChat';
-import { CHAT_CONTENT, GET_INFO } from '../../action/detailChat/interface';
+import { getChatInfo, getDetailChat, getInfo } from '../../../util/api/detailChat';
+import { CHAT_CONTENT, GET_CHAT_INFO, GET_INFO } from '../../action/detailChat/interface';
 import { reducerType } from '../../reducer';
 import DetailChatState from '../../reducer/detailChat/interface';
 
@@ -67,9 +67,41 @@ const infoGetSaga = function* (): any {
   }
 };
 
+const chatInfoGetSaga = function* (): any {
+  const actionType = 'DETAILCHAT/GET_CHAT_INFO';
+  const SUCCESS = `${actionType}_SUCCESS`;
+  const FAILURE = `${actionType}_FAILURE`;
+  const state = yield select(getStateFunc);
+  const accessToken = localStorage.getItem('access_token') || '';
+  try {
+    const response = yield call(getChatInfo, accessToken, state.roomId);
+    yield put({
+      type: SUCCESS,
+      payload: response ? response.data : null,
+    });
+  } catch (error) {
+    if (error.response?.data) {
+      yield put({
+        type: FAILURE,
+        payload: { ...error.response.data, type: actionType },
+      });
+    } else {
+      console.log(error);
+      yield put({
+        type: FAILURE,
+        payload: {
+          message: 'Network Error',
+          status: 500,
+        },
+      });
+    }
+  }
+};
+
 function* detailChatSaga() {
   yield takeLatest(CHAT_CONTENT, detailChatGetSaga);
   yield takeLatest(GET_INFO, infoGetSaga);
+  yield takeLatest(GET_CHAT_INFO, chatInfoGetSaga);
 }
 
 export default detailChatSaga;
