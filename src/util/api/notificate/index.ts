b93a0cf47d2c3fbde90e176cfc2f6ea25dda21bf
김예진 <wlsdong0004@gmail.com>
2021-11-30 07:47:56 +0900

    [ADD] 문의사항 답변 보기 api 작성

--- src/util/api/notificate/index.ts
+++ src/util/api/notificate/index.ts
@@ -48,7 +48,17 @@ export default {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-      data: { Notification: notiId },
+      data: {},
+    });
+  },
+  setQuestionAnswer(accessToken: string | null, id: number | string) {
+    return request({
+      url: `/question?question_id=${id}`,
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: {},
     });
   },
 };
