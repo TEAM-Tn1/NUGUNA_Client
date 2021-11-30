04706be57aa99b8f2de7a6ebe06b926286aeb5ab
seonghyeon <shp35079@naver.com>
2021-11-30 19:09:04 +0900

    [ADD] 내가 쓴 게시물 api 연동

--- src/components/mypage/mypageOptions/writtenPost/WrittenPost.tsx
+++ src/components/mypage/mypageOptions/writtenPost/WrittenPost.tsx
@@ -1,21 +1,64 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import OptionsTemplate from '../../../default/profile/optionsTemplate';
 import Footer from '../../../footer';
 import Header from '../../../header';
 import { written } from '../../../../assets/mypage';
 import * as S from './style';
 import PostContent from '../../../post/PostContent';
+import mypage from '../../../../util/api/mypage';
+import { useInView } from 'react-intersection-observer';
 
 const WrittenPost = () => {
   const [isShow, setIsShow] = useState(true);
-  const [isGroup, setIsGroup] = useState(false);
+  const [postType, setPostType] = useState('trade');
+
+  const [list, setList] = useState<Array<any>>([]);
+  const [lastCheck, setLastCheck] = useState<boolean>();
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [ref, inView] = useInView();
+  const { getUserPost } = mypage;
+
+  const getList = useCallback(async () => {
+    setLoading(true);
+    setLastCheck(false);
+    await getUserPost(localStorage.getItem('email'), page, postType)
+      .then(res => {
+        if (res.data.length === 0) {
+          setLastCheck(true);
+        }
+        setList(prevState => [...prevState, ...res.data]);
+      })
+      .catch(err => {
+        throw err;
+      });
+    setLoading(false);
+  }, [page, postType]);
+
+  useEffect(() => {
+    getList();
+  }, [page, postType]);
+
+  useEffect(() => {
+    setList([]);
+    setPage(0);
+    window.scrollTo({
+      top: 0,
+    });
+  }, [postType]);
+
+  useEffect(() => {
+    if (inView && !loading && !lastCheck) {
+      setPage(prevState => prevState + 1);
+    }
+  }, [inView, loading]);
 
   const setCarrot = () => {
-    setIsGroup(false);
+    setPostType('trade');
   };
 
   const setGroup = () => {
-    setIsGroup(true);
+    setPostType('group');
   };
 
   return (
@@ -29,26 +72,38 @@ const WrittenPost = () => {
         isShow={isShow}
       />
       {isShow && (
-        <S.ListWrapper>
-          <S.TypeSelector isGroup={isGroup}>
+        <>
+          <S.TypeSelector postType={postType}>
             <p onClick={setCarrot}>거래</p>
             <p onClick={setGroup}>공구</p>
           </S.TypeSelector>
-          <PostContent
-            key={1}
-            feedId={1}
-            medium={''}
-            title={'dasdsa'}
-            money={1000}
-            like={3}
-            type={''}
-            date={''}
-            people={''}
-            hashtag={[]}
-            isLikeClick={false}
-          />
-        </S.ListWrapper>
+          <S.ListWrapper>
+            {list.map(ele => {
+              const month = ele.date && ele.date.slice(5, 7);
+              const date = ele.date && ele.date.slice(8);
+              const dates = `${month}/${date}`;
+              const showPeople = `${ele.current_head_count}/${ele.head_count}`;
+              return (
+                <PostContent
+                  key={ele.feed_id}
+                  feedId={ele.feed_id}
+                  medium={ele.medium}
+                  title={ele.title}
+                  money={ele.price}
+                  like={ele.count}
+                  type={postType}
+                  date={dates}
+                  people={showPeople}
+                  hashtag={ele.tags}
+                  isLikeClick={ele.like}
+                />
+              );
+            })}
+            {!loading && <p ref={ref}>로딩 중...</p>}
+          </S.ListWrapper>
+        </>
       )}
+
       <Footer />
     </S.Wrapper>
   );
--- src/components/mypage/mypageOptions/writtenPost/style/index.ts
+++ src/components/mypage/mypageOptions/writtenPost/style/index.ts
@@ -1,7 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 
 interface Props {
-  isGroup: boolean;
+  postType: string;
 }
 
 export const Wrapper = styled.div`
@@ -25,12 +25,22 @@ export const ListWrapper = styled.div`
   opacity: 0;
   animation: ${fadeIn} 0.25s ease 0.75s;
   animation-fill-mode: forwards;
+  padding: 40px 0;
+
+  & > p {
+    opacity: 0;
+  }
 `;
 
 export const TypeSelector = styled.div<Props>`
+  width: 100%;
   display: flex;
   gap: 10px;
   padding: 10px 30px;
+  position: fixed;
+  top: 160px;
+  background-color: white;
+  z-index: 1;
 
   & > p {
     font-size: 11px;
@@ -38,10 +48,10 @@ export const TypeSelector = styled.div<Props>`
   }
 
   & > p:first-child {
-    opacity: ${props => (props.isGroup ? '0.5' : '1')};
+    opacity: ${props => (props.postType === 'trade' ? '1' : '0.5')};
   }
 
   & > p:last-child {
-    opacity: ${props => (props.isGroup ? '1' : '0.5')};
+    opacity: ${props => (props.postType === 'group' ? '1' : '0.5')};
   }
 `;
